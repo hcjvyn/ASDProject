efaf26ce9fab225435177f58f7ead5cbd7f0f464
Yen <yen.r.plagata@gmail.com>
2014-04-22 14:28:36 -0500

    add acc dialog

--- src/framework/ui/AAddAccDialog.java
+++ src/framework/ui/AAddAccDialog.java
@@ -11,60 +11,6 @@ public abstract class AAddAccDialog extends JDialog {
 	protected ICustomer customer;
 	
 	public AAddAccDialog() {
-		JLabel1.setText("Name");
-		getContentPane().add(JLabel1);
-		JLabel1.setForeground(java.awt.Color.black);
-		JLabel1.setBounds(12,84,48,24);
-		
-		JLabel2.setText("Street");
-		getContentPane().add(JLabel2);
-		JLabel2.setForeground(java.awt.Color.black);
-		JLabel2.setBounds(12,108,48,24);
-		
-		JLabel3.setText("City");
-		getContentPane().add(JLabel3);
-		JLabel3.setForeground(java.awt.Color.black);
-		JLabel3.setBounds(12,132,48,24);
-		
-		JLabel4.setText("State");
-		getContentPane().add(JLabel4);
-		JLabel4.setForeground(java.awt.Color.black);
-		JLabel4.setBounds(12,156,48,24);
-		
-		JLabel5.setText("Zip");
-		getContentPane().add(JLabel5);
-		JLabel5.setForeground(java.awt.Color.black);
-		JLabel5.setBounds(12,180,48,24);
-		
-		JButton_OK.setText("OK");
-		JButton_OK.setActionCommand("OK");
-		getContentPane().add(JButton_OK);
-		JButton_OK.setBounds(48,264,84,24);
-		
-		JButton_Cancel.setText("Cancel");
-		JButton_Cancel.setActionCommand("Cancel");
-		getContentPane().add(JButton_Cancel);
-		JButton_Cancel.setBounds(156,264,84,24);
-		
-		getContentPane().add(JTextField_NAME);
-		JTextField_NAME.setBounds(84,84,156,20);
-		getContentPane().add(JTextField_CT);
-		JTextField_CT.setBounds(84,132,156,20);
-		getContentPane().add(JTextField_ST);
-		JTextField_ST.setBounds(84,156,156,20);
-		getContentPane().add(JTextField_STR);
-		JTextField_STR.setBounds(84,108,156,20);
-		getContentPane().add(JTextField_ZIP);
-		JTextField_ZIP.setBounds(84,180,156,20);
-		
-		getContentPane().add(JTextField_ACNR);
-		JTextField_ACNR.setBounds(84,60,156,20);
-		JLabel8.setText("Acc Nr");
-		getContentPane().add(JLabel8);
-		
-		JLabel8.setForeground(java.awt.Color.black);
-		JLabel8.setBounds(12,60,48,24);
-		
 		//{{REGISTER_LISTENERS
 		SymAction lSymAction = new SymAction();
 		JButton_OK.addActionListener(lSymAction);
@@ -84,8 +30,8 @@ public abstract class AAddAccDialog extends JDialog {
 	protected javax.swing.JTextField JTextField_STR = new javax.swing.JTextField();
 	protected javax.swing.JTextField JTextField_ZIP = new javax.swing.JTextField();
 	
-	javax.swing.JTextField JTextField_ACNR = new javax.swing.JTextField();
-	javax.swing.JLabel JLabel8 = new javax.swing.JLabel();
+	protected javax.swing.JTextField JTextField_ACNR = new javax.swing.JTextField();
+	protected javax.swing.JLabel JLabel8 = new javax.swing.JLabel();
 	
 	protected javax.swing.JButton JButton_OK = new javax.swing.JButton();
 	protected javax.swing.JButton JButton_Cancel = new javax.swing.JButton();
