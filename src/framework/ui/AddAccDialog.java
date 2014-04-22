cb96af0d61465ea059c8c0025ad4a49ebfb0f5c2
Yen <yen.r.plagata@gmail.com>
2014-04-22 13:50:14 -0500

    AccDialog boxes

--- src/framework/ui/AddAccDialog.java
+++ src/framework/ui/AddAccDialog.java
@@ -1,12 +1,29 @@
 package framework.ui;
 
+import framework.account.factory.AccountFactory;
+import framework.account.factory.IAccountFactory;
+import framework.customer.Customer;
 import framework.customer.ICustomer;
 
-public abstract class AddAccDialog extends javax.swing.JDialog {
+public class AddAccDialog extends AAddAccDialog {
 
-	protected ICustomer customer;
-	public ICustomer getCustomer() {
-		return customer;
+	protected ICustomer createCustomer() {
+		String clientName = JTextField_NAME.getText();
+		String street = JTextField_STR.getText();
+		String city = JTextField_CT.getText();
+		String zip = JTextField_ZIP.getText();
+		String state = JTextField_ST.getText();
+		
+		return new Customer(
+				clientName,
+				street,
+				city,
+				state,
+				zip);
+	}
+	
+	protected IAccountFactory selectFactory() {
+		return new AccountFactory();
 	}
 
 }
